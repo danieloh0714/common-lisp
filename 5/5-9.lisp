7c96c607de26b4e0681e6891f372e93865b6b011
Danny Oh <dannyoh0316@gmail.com>
2021-10-18 13:36:20 -0500

    update 5-9

--- 5/5-9.lisp
+++ 5/5-9.lisp
@@ -1,46 +1,40 @@
 ;; a
 (defun new-paths (path node end net)
-  (mapcar #'(lambda (n)
-              (let ((new-path (cons n path)))
-                (cond ((eql n end) (throw 'abort (reverse new-path)))
-                      ((member n path) nil)
-                      (t new-path))))
+  (mapcan #'(lambda (n)
+              (cond ((eql n end) (throw 'abort (reverse (cons n path))))
+                    ((member n path) nil)
+                    (t (list (cons n path)))))
           (cdr (assoc node net))))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
-      (let ((path (car queue)))
-        (let ((node (car path)))
-          (bfs end
-               (append (cdr queue)
-                       (new-paths path node end net))
-               net)))))
+      (let* ((path (car queue)) (node (car path)))
+        (bfs end
+             (append (cdr queue)
+                     (new-paths path node end net))
+             net))))
 
 (defun shortest-path (start end net)
   (catch 'abort (bfs end (list (list start)) net)))
 
 ;; b
-(defun new-paths (path node end net)
-  (mapcar #'(lambda (n)
-              (let ((new-path (cons n path)))
-                (cond ((eql n end) (return-from new-paths new-path))
-                      ((member n path) nil)
-                      (t new-path))))
-          (cdr (assoc node net))))
+(defun new-paths (path neighbors)
+  (mapcan #'(lambda (n)
+              (if (member n path)
+                  nil
+                  (list (cons n path))))
+          neighbors))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
-      (let ((path (car queue)))
-        (let ((node (car path)))
-          (let ((new-path (new-paths path node end net)))
-            (if (eql end (car new-path))
-                (reverse new-path)
-                (bfs end
-                     (append (cdr queue)
-                             (new-paths path node end net))
-                     net)))))))
+      (let* ((path (car queue)) (node (car path)) (neighbors (cdr (assoc node net))))
+        (if (member end neighbors)
+            (reverse (cons end path))
+            (bfs end
+                 (append (cdr queue) (new-paths path neighbors))
+                 net)))))
 
 (defun shortest-path (start end net)
   (bfs end (list (list start)) net))
